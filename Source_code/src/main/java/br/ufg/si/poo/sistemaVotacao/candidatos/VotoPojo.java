048dba7639347a7880c9a2546a8efa7c89006525
Bruno Nogueira de Oliveira <ti.brunonogueira@gmail.com>
2015-05-12 18:26:38 -0300

    [Nova #5] Criando objeto POJO para voto;
    Signed-off-by: Bruno Nogueira de Oliveira <ti.brunonogueira@gmail.com>
    
     No ramo bruno-nova-5
     Mudanças a serem submetidas:
    	new file:   Source_code/src/main/java/br/ufg/si/poo/sistemaVotacao/candidatos/VotoPojo.java

---
+++ Source_code/src/main/java/br/ufg/si/poo/sistemaVotacao/candidatos/VotoPojo.java
@@ -0,0 +1,66 @@
+package br.ufg.si.poo.sistemaVotacao.candidatos;
+/**
+ * @author Bruno Nogueira<br>
+ *
+ * <p>Classe representa um objeto voto;
+ *
+ */
+public class VotoPojo {
+
+	private DeputadosFederais deputadoFederal;
+	private DeputadosEstaduais deputadoEstadual;
+	private Senadores senador;
+	private Governadores governador;
+	private Presidentes presidente;
+
+	/**
+	 * Construtor padrao da Classe VotoPojo
+	 * @param deputadoFederal
+	 * @param deputadoEstadual
+	 * @param senador
+	 * @param governador
+	 * @param presidente
+	 */
+	public VotoPojo(DeputadosFederais deputadoFederal,
+			DeputadosEstaduais deputadoEstadual, Senadores senador,
+			Governadores governador, Presidentes presidente) {
+		this.deputadoFederal = deputadoFederal;
+		this.deputadoEstadual = deputadoEstadual;
+		this.senador = senador;
+		this.governador = governador;
+		this.presidente = presidente;
+	}
+
+	/**
+	 * @return the deputadoFederal
+	 */
+	public DeputadosFederais getDeputadoFederal() {
+		return deputadoFederal;
+	}
+	/**
+	 * @return the deputadoEstadual
+	 */
+	public DeputadosEstaduais getDeputadoEstadual() {
+		return deputadoEstadual;
+	}
+	/**
+	 * @return the senador
+	 */
+	public Senadores getSenador() {
+		return senador;
+	}
+	/**
+	 * @return the governador
+	 */
+	public Governadores getGovernador() {
+		return governador;
+	}
+	/**
+	 * @return the presidente
+	 */
+	public Presidentes getPresidente() {
+		return presidente;
+	}
+
+
+}
