eb87c08e94eafa0bdef39583a4428fd9fc656261
Bruno Nogueira de Oliveira <ti.brunonogueira@gmail.com>
2015-05-13 22:20:39 -0300

    [Nova #6] Melhorando lógica do gerador de arquivos para criar o arquivo TXT se o mesmo não existir;
    Signed-off-by: Bruno Nogueira de Oliveira <ti.brunonogueira@gmail.com>

--- Source_code/src/main/java/br/ufg/si/poo/sistemaVotacao/manipuladorArquivos/ManipuladorArquivoImpl.java
+++ Source_code/src/main/java/br/ufg/si/poo/sistemaVotacao/manipuladorArquivos/ManipuladorArquivoImpl.java
@@ -1,6 +1,7 @@
 package br.ufg.si.poo.sistemaVotacao.manipuladorArquivos;
 
 import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 
@@ -10,14 +11,25 @@ public class ManipuladorArquivoImpl implements IManipuladorArquivo {
 
 	public void gravarVoto(VotoPojo voto) {
 		try {
-			BufferedWriter buffWrite = new BufferedWriter(new FileWriter(NOME_ARQUIVO));
+			BufferedWriter buffWrite = new BufferedWriter( criarFileWriter() );
 			String linha = voto.toString();
 			buffWrite.append(linha + "\n");
 			buffWrite.close();
 
 		} catch (IOException e) {
-			System.err.println("Problema ao abrir buffer de escrita");
+			System.err.println("Problema ao escrever registro: " + e);
 		}
+
+	}
+
+	private FileWriter criarFileWriter() throws IOException {
+
+		File arquivo = new File(NOME_ARQUIVO);
+		if(!arquivo.exists()) {
+			arquivo.createNewFile();
+		}
+		FileWriter file = new FileWriter(arquivo);
+		return file;
 
 	}
 
